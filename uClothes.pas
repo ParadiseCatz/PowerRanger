53e1143b63ff03cc9c6220e72242a1b03f6ed9f6
ParadiseCatz <ChristianAnthonySetyawan@gmail.com>
2015-04-08 10:06:43 +0700

    Create Clothes Unit

---
+++ uClothes.pas
@@ -0,0 +1,23 @@
+unit uClothes;
+
+interface
+	
+	type
+		Clothes = Object
+			name : String;
+			category : String;
+			gender : String;
+			colour : String;
+			weight : real;
+			material : String;
+			price_per_kg : real;
+			
+			procedure tes;
+		end;
+
+implementation
+	procedure Clothes.tes;
+	begin
+	
+	end;
+end.
