7c530df243c5dd340a25bd0a17962ad47460e8fe
ParadiseCatz <ChristianAnthonySetyawan@gmail.com>
2015-04-08 13:07:15 +0700

    Create Courier unit

---
+++ uCourier.pas
@@ -0,0 +1,21 @@
+unit uCourier;
+
+interface
+	
+	type
+		Courier = Object
+			name : String;
+			type_of_service : String;
+			destination_city : String;
+			price_per_kg : String;
+			delivery_time : real;
+			
+			procedure tes;
+		end;
+
+implementation
+	procedure Clothes.tes;
+	begin
+	
+	end;
+end.
