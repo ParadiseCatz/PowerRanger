4f38acdb409371f9f68531e404343e84b3666be6
ParadiseCatz <ChristianAnthonySetyawan@gmail.com>
2015-04-09 00:04:32 +0700

    Add string to date parser

--- uParser.pas
+++ uParser.pas
@@ -1,10 +1,13 @@
 unit uParser; //Service
 
 interface
+	uses uDate;
+
 	type
 		arString = array[1..1000] of string;
 		Parser = Object
 			function stringToArray(s:ansistring):arString;
+			function stringToDate(s:ansistring):Date;
 		end;
 implementation
 	function Parser.stringToArray(s:ansistring):arString;
@@ -25,5 +28,28 @@ implementation
 		end;
 		copy(returnArray[indexArray], posAkhir, length(s)-posAkhir);
 		stringToArray := returnArray;
+	end;
+	function Parser.stringToDate(s:ansistring):Date;
+	var
+		i, posAkhir, indexArray:longint;
+		returnArray : array[0..50] of integer;
+		returnDate : Date;
+	begin
+		indexArray := 1;
+		posAkhir := 1;
+		for i:=1 to length(s) do
+		begin
+			if (s[i] = '/') then
+			begin
+				val(copy(s, posAkhir, i-posAkhir), returnArray[indexArray]);
+				posAkhir := i+1;
+				indexArray := indexArray +1;
+			end;
+		end;
+		val(copy(s, posAkhir, i-posAkhir), returnArray[indexArray]);
+		returnDate.day := returnArray[1];
+		returnDate.month := returnArray[2];
+		returnDate.year := returnArray[3];
+		stringToDate := returnDate;
 	end;
 end.
