7becf3173eb719e96c8683b11c2a4ab6192233f8
ParadiseCatz <ChristianAnthonySetyawan@gmail.com>
2015-04-08 22:05:09 +0700

    Create Config unit

---
+++ uConfig.pas
@@ -0,0 +1,11 @@
+unit uConfig;
+
+interface
+	const
+		DATABASE_CLOTHES_FILENAME = 'database_clothes.txt';
+		DATABASE_COURIER_FILENAME = 'database_courier.txt';
+		DATABASE_SHOPPING_CART_FILENAME = 'database_shopping_cart.txt';
+		DATABASE_TRANSACTION_FILENAME = 'database_transaction.txt';
+implementation
+
+end.
