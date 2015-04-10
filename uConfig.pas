ae3b2f0df2ffb6867b01ec4e1c0bd204fdc9125d
ParadiseCatz <ChristianAnthonySetyawan@gmail.com>
2015-04-10 20:54:58 +0700

    Add command list constant

--- uConfig.pas
+++ uConfig.pas
@@ -2,11 +2,29 @@ unit uConfig;
 
 interface
 	const
-		ARRAY_LIMIT = 55;
+		ARRAY_LIMIT = 30;
 		DATABASE_CLOTHES_FILENAME = 'database_clothes.txt';
 		DATABASE_COURIER_FILENAME = 'database_courier.txt';
 		DATABASE_SHOPPING_CART_FILENAME = 'database_shopping_cart.txt';
 		DATABASE_TRANSACTION_FILENAME = 'database_transaction.txt';
+		commandList : array[1..17] of string = ('load', 
+												'showpopulars', 
+												'showdetailproduct', 
+												'searchclothesbykeyword', 
+												'sortprice', 
+												'filterclothes',
+												'filterbyprice',
+												'showexpedition',
+												'addtocart',
+												'removefromcart',
+												'calculateprice',
+												'checkout',
+												'updateclothes',
+												'discountgrosir',
+												'showtransaction',
+												'retur',
+												'exit'
+												);
 implementation
 
 end.
