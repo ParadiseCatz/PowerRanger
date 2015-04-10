f499601136dbceda2543d918143eb41c441f9678
ParadiseCatz <ChristianAnthonySetyawan@gmail.com>
2015-04-10 19:15:02 +0700

    Add Save All procedure

--- main.pas
+++ main.pas
@@ -1,6 +1,6 @@
 program OnlineShopping;
 
-uses uLoader, uTransactionPool, uShoppingCart, uCourierPool, uWarehouse, uConfig;
+uses uLoader, uTransactionPool, uShoppingCart, uCourierPool, uWarehouse, uConfig, uSaver;
 
 var
 	userCommand : string;
@@ -15,6 +15,14 @@ begin
 	mainCourierPool := loadCourier(DATABASE_COURIER_FILENAME);
 	mainShoppingCart := loadShoppingCart(DATABASE_SHOPPING_CART_FILENAME);
 	mainTransactionPool := loadTransaction(DATABASE_TRANSACTION_FILENAME);
+end;
+
+procedure saveAll();
+begin
+	saveWarehouse(mainWarehouse,DATABASE_CLOTHES_FILENAME);
+	saveCourier(mainCourierPool,DATABASE_COURIER_FILENAME);
+	saveShoppingCart(mainShoppingCart,DATABASE_SHOPPING_CART_FILENAME);
+	saveTransaction(mainTransactionPool,DATABASE_TRANSACTION_FILENAME);
 end;
 
 procedure userGreeter();
