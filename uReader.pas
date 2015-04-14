a52482afa5887a74379987defe4d38ef53cf7c68
ParadiseCatz <ChristianAnthonySetyawan@gmail.com>
2015-04-14 19:32:08 +0700

    Add Reader unit

---
+++ uReader.pas
@@ -0,0 +1,24 @@
+unit uReader;
+
+interface
+	uses uClothes, uWarehouse, uShoppingCartItem;
+
+	procedure readClothesByName(var c:Clothes; ws:Warehouse);
+	procedure readQuantity(var sci:ShoppingCartItem);
+implementation
+	procedure readClothesByName(var c:Clothes; ws:Warehouse);
+	var
+		name:string;
+	begin
+		write('Please enter clothes name: '); readln(name);
+		c := warehouseFindByName(name, ws);
+	end;
+
+	procedure readQuantity(var sci:ShoppingCartItem);
+	begin
+		write('S size quantity: '); readln(sci.s_quantity);
+		write('M size quantity: '); readln(sci.m_quantity);
+		write('L size quantity: '); readln(sci.l_quantity);
+		write('XL size quantity: '); readln(sci.xl_quantity);
+	end;
+end.
