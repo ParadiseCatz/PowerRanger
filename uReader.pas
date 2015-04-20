494031491c9413687bf93b01c9842a781683257c
lolipopper <njruntuwene@gmail.com>
2015-04-20 18:34:18 +0900

    Added readCourier

--- uReader.pas
+++ uReader.pas
@@ -1,10 +1,11 @@
 unit uReader;
 
 interface
-	uses uClothes, uWarehouse, uShoppingCartItem;
+	uses uClothes, uWarehouse, uShoppingCartItem, uDate, uCourier, uParser;
 
 	procedure readClothesByName(var c:Clothes; ws:Warehouse);
 	procedure readQuantity(var sci:ShoppingCartItem);
+
 implementation
 	procedure readClothesByName(var c:Clothes; ws:Warehouse);
 	var
@@ -20,5 +21,21 @@ implementation
 		write('M size quantity: '); readln(sci.m_quantity);
 		write('L size quantity: '); readln(sci.l_quantity);
 		write('XL size quantity: '); readln(sci.xl_quantity);
+	end;
+
+	procedure readDate(var d:Date);
+	var
+		s:string;
+	begin
+		readln(s);
+		d:=stringToDate(s);
+	end;
+
+	procedure readCourier(var co:Courier,var d:Date);
+	begin
+		write('Nama Ekspsedisi : '); readln(co.name);
+		write('Jenis Layanan : '); readln(co.type_of_service);
+		write('Kota Tujuan : '); readln(co.destination_city);
+		write('Tanggal Kirim : '); readDate(d);
 	end;
 end.
